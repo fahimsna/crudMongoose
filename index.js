2032e15c44b36774b658009854951ca5d7719c03
Fahim Shahriar Nur <shahriarfahim091@gmail.com>
2026-08-03 11:46:06 +0600

    Added insert api

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 let express = require("express");
 let mongoose = require("mongoose");
+let enquiryModel = require("./models/enquiry.model");
 require("dotenv").config();
 
 //connect to MongoDb
 
 let app = express();
+app.use(express.json());
+app.post("/api/enquiry-insert", (req, res) => {
+  let { sName, sEmail, sPhone, sMessage } = req.body;
+  let enquiry = new enquiryModel({
+    name: sName,
+    email: sEmail,
+    phone: sPhone,
+    message: sMessage,
+  });
+  enquiry
+    .save()
+    .then(() => {
+      res.send({ status: 1, message: "Enquiry Saved Successfully" });
+    })
+    .catch((error) => {
+      res.send({ status: 0, message: "Error while saving Enquiry",error:error });
+    });
+});
 
-
+app.get("/api/enquiry-list",(req,res)=>{
+  res.send({status:1,message:"Enquiry List",data:[]})
+})
 
 mongoose.connect(process.env.DBURL).then(() => {
   console.log("Connected to MongoDb");
-  app.listen(process.env.PORT,(()=>{
+  app.listen(process.env.PORT, () => {
     console.log("Server is running on port " + process.env.PORT);
-  }));
+  });
 });
