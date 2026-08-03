b77c98e2aeab2b88d20f2852fc6878b0a0d4e5f1
Fahim Shahriar Nur <shahriarfahim091@gmail.com>
2026-08-03 12:08:35 +0600

    Added Delete API

--- index.js
+++ index.js
@@ -21,14 +21,29 @@ app.post("/api/enquiry-insert", (req, res) => {
       res.send({ status: 1, message: "Enquiry Saved Successfully" });
     })
     .catch((error) => {
-      res.send({ status: 0, message: "Error while saving Enquiry",error:error });
+      res.send({
+        status: 0,
+        message: "Error while saving Enquiry",
+        error: error,
+      });
     });
 });
 
-app.get("/api/enquiry-list",async(req,res)=>{
-  let enquireList=await enquiryModel.find();
-  res.send({status:1,message:"Enquiry List",data:enquireList})
-})
+app.get("/api/enquiry-list", async (req, res) => {
+  let enquireList = await enquiryModel.find();
+  res.send({ status: 1, message: "Enquiry List", data: enquireList });
+});
+
+app.delete("/api/enquiry-delete/:id", async (req, res) => {
+  let enquiryId = req.params.id;
+  let deletedEnquiry=await enquiryModel.deleteOne({_id:enquiryId});
+  res.send({
+    status: 1,
+    message: "Enquiry Deleted Successfully",
+    id: enquiryId,
+    delRes:deletedEnquiry
+  });
+});
 
 mongoose.connect(process.env.DBURL).then(() => {
   console.log("Connected to MongoDb");
